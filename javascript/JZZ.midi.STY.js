594ee9053c8e441bd1ca2ddfbe425ccbb9b46f06
Sema <jazz-dev@jazz-soft.net>
2023-04-14 21:57:38 -0400

    Added npm project

---
+++ javascript/JZZ.midi.STY.js
@@ -0,0 +1,18 @@
+(function(global, factory) {
+  /* istanbul ignore next */
+  if (typeof exports === 'object' && typeof module !== 'undefined') {
+    factory.STY = factory;
+    module.exports = factory;
+  }
+  else if (typeof define === 'function' && define.amd) {
+    define('JZZ.gui.STY', ['JZZ', 'JZZ.midi.SMF'], factory);
+  }
+  else {
+    factory(JZZ);
+  }
+})(this, function(JZZ) {
+
+  /* istanbul ignore next */
+  if (JZZ.MIDI.STY) return;
+
+});
